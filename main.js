6677c0d07a58198660f752db113e84b073df3ba6
max_shirokov <maksim.shirokov2011@mail.ru>
2021-06-07 13:54:54 +0300

    l3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const products = [
+/* const products = [
     { id: 1, title: 'Notebook', price: 1000 },
     { id: 2, title: 'Mouse', price: 100 },
     { id: 3, title: 'Keyboard', price: 250 },
@@ -23,4 +23,68 @@ const renderProducts = (list = []) => {
     })
 }
 
-renderProducts(products);
+renderProducts(products); */
+
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class Catalog {
+    constructor(container) {
+        this.container = container;
+
+        this.goods = [];
+
+        this.allProducts = [];
+
+        this.getRequest(`${API}/catalogData.json`)
+            .then(data => {
+                this.allProducts = JSON.parse(data);
+                this.renderCatalog()
+            })
+    }
+
+
+    getRequest(url) {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', url, true);
+            xhr.onload = () => {
+                if (xhr.readyState === 4) {
+                    if (xhr.status !== 200) {
+                        reject(xhr.response);
+                    } else {
+                        resolve(xhr.response)
+                    }
+                }
+            }
+            xhr.send()
+
+
+        })
+    }
+
+    renderCatalog() {
+        const containerField = document.querySelector(`${this.container}`);
+
+        this.allProducts.forEach(element => containerField.insertAdjacentHTML('beforeend', new Item(element).render()))
+    }
+
+};
+
+class Item {
+    constructor(element) {
+        this.element = element;
+
+        this.render()
+    }
+
+    render() {
+        return `<div class="product-item">
+                <h3>${this.element.id_product}</h3>
+                <p>${this.element.price}</p>
+                <button class="by-btn">Добавить</button>
+              </div>`;
+    }
+}
+
+let catalog = new Catalog('.products')
+
